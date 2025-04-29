179b7e5c04730e96f60c33122436eafc4a809117
Rodrigo Ferreira <rodrigo.a.f@icloud.com>
2025-04-29 09:52:18 -0300

    adicionado log4j

--- src/main/java/com/rodrigoferreira/to_do_list/service/TodoService.java
+++ src/main/java/com/rodrigoferreira/to_do_list/service/TodoService.java
@@ -2,11 +2,14 @@ package com.rodrigoferreira.to_do_list.service;
 
 import java.util.List;
 
+
 import org.springframework.stereotype.Service;
-import com.rodrigoferreira.to_do_list.ToDoListApplication;
 import com.rodrigoferreira.to_do_list.model.Todo;
 import com.rodrigoferreira.to_do_list.repository.TodoRepository;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Service
 public class TodoService {
 
@@ -22,20 +25,28 @@ public class TodoService {
 	
 	public List<Todo> create(Todo todo){
 		todoRepository.save(todo);
+		log.info("Nova tarefa salva no banco de dados {}", todo);
 		return listTasks();
 	}
 	
 	public List<Todo> update(Todo todo){
+		if (!todoRepository.existsById(todo.getId())) {
+			log.warn("Tentativa de exclusão de tarefa inexistente ID: {}", todo.getId());
+			throw new RuntimeException("Tarefa não encontrada para atualização.");
+		}
 		todoRepository.save(todo);
+		log.info("Tarefa atualizada no banco de dados {}.", todo);
 		return listTasks();
 	}
 	
 	public List<Todo> delete(Long id) {
 	    if (!todoRepository.existsById(id)) {
-	        throw new RuntimeException("Tarefa não encontrada para exclusão");
+	    	log.warn("Tentatia de excluir tarefa com ID: {}.", id);
+	        throw new RuntimeException("Tarefa não encontrada para exclusão.");
 	    }
 
 	    todoRepository.deleteById(id);
+	    log.info("Tarefa com o ID: {} excluída com sucesso.", id);
 	    return listTasks();
 	}
 	
