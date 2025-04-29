57817f0ddfcedd4c68ec0eb9e46c5e13e57b1493
Rodrigo Ferreira <rodrigo.a.f@icloud.com>
2025-04-29 13:20:46 -0300

    documentação com swagger

--- src/main/java/com/rodrigoferreira/to_do_list/controller/TodoController.java
+++ src/main/java/com/rodrigoferreira/to_do_list/controller/TodoController.java
@@ -13,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rodrigoferreira.to_do_list.model.Todo;
 import com.rodrigoferreira.to_do_list.service.TodoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/todo")
+@Tag(name = "Tarefas", description = "Operaçoes relacionadas a tarefas.")
 public class TodoController {
 	
 	TodoService todoService;
@@ -25,21 +29,32 @@ public class TodoController {
 		this.todoService = todoService;
 	}
 
+	@Operation(summary = "Listar Tarefas", description = "Retorna todos as tarefas cadastradas no sistema.")
+	@ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
 	@GetMapping
 	public List<Todo> listTasks() {
 		return todoService.listTasks();
 	}
 	
+	@Operation(summary = "Cadastra uma tarefa", description = "Cadastra uma nova tarefa no sistema e retorna uma lista atualizada.")
+	@ApiResponse(responseCode = "200", description = "Tarefa cadastrada com sucesso.")
+	@ApiResponse(responseCode = "400", description = "Erro ao cadastrar uma nova tarefa.")
 	@PostMapping
 	public List<Todo> createTask(@RequestBody @Valid Todo todo) {
 		return todoService.create(todo);
 	}
 	
+	@Operation(summary = "Atualizar Tarefa", description = "Atualizar uma nova tarefa no sistema e retorna uma lista atualizada.")
+	@ApiResponse(responseCode = "200", description = "Tarefa atualizada com sucesso.")
+	@ApiResponse(responseCode = "400", description = "Erro ao atualziar uma nova tarefa.")
 	@PutMapping
 	public List<Todo> updateTask(@RequestBody @Valid Todo todo) {
 		return todoService.update(todo);
 	}
 	
+	@Operation(summary = "Deletar Tarefa", description = "Deletar Tarefa por ID.")
+	@ApiResponse(responseCode = "200", description = "Tarefa deletada com sucesso.")
+	@ApiResponse(responseCode = "500", description = "Erro ao atualziar uma nova tarefa.")
 	@DeleteMapping("/{id}")
 	public List<Todo> deleteTask(@PathVariable Long id) {
 		return todoService.delete(id);
